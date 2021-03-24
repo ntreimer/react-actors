a74394449b51f5324d20034ccf497ae26f013565
Nate Treimer <natetreimer@gmail.com>
2021-03-24 10:14:18 -0500

    put input fields on DOM

--- react-actors/src/components/Contact/Contact.jsx
+++ react-actors/src/components/Contact/Contact.jsx
@@ -4,6 +4,10 @@ function Contact() {
     return (
         <div>
             <h2>Contact</h2>
+            <input type="text" placeholder="First Name"/>
+            <input type="text" placeholder="Last Name"/>
+            <input type="text" placeholder="Best impression"/>
+            <button type="submit">Join us!</button>
             <p>Phone number: 1-800-NICK-AGE</p>
             <p>E-mail: actors@acting.com</p>
         </div>
